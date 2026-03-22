00d30b249c1be0fc9e0427811d78e80548034fe1
Ethan Vaiz <edanvaiz@gmail.com>
2026-03-21 23:30:03 -0500

    Column Name

--- pages/Sign Up.py
+++ pages/Sign Up.py
@@ -24,7 +24,7 @@ users_dict = users_ref.get()
 def user_location_list() -> list[str]:
     csv_path = "uscities.csv"
     final_list = []
-    df = pandas.read_csv(csv_path, usecols = [0, 3])
+    df = pandas.read_csv(csv_path, usecols = ["city", "state_name"])
     for i in df.index:
         final_list.append(f"{df.loc[i, "city"]}, {df.loc[i, "state_name"]}")
     
