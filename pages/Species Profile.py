9abcf94b11d955cca9c9b22e380a114189d78b61
Ethan Vaiz <edanvaiz@gmail.com>
2026-03-25 19:58:00 -0500

    Bird Scraping Fixing

--- pages/Species Profile.py
+++ pages/Species Profile.py
@@ -73,7 +73,7 @@ if not find_hero_wrap:
     st.page_link(label = "Wikipedia", page = f"https://en.wikipedia.org/wiki/{first_title}", icon = ":material/info:")
 
 else:
-    image_link = find_hero_wrap.get("style").split("(\"")[1].split("\")")[0]
+    image_link = find_hero_wrap[0].get("style").split("(\"")[1].split("\")")[0]
 
     st.title(st.query_params.species)
     st.badge(label = st.query_params.type, color = color_map[st.query_params.type])
